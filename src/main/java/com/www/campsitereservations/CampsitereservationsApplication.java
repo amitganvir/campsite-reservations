c4ede30e38af8bf99337e71a907c70d8c6bfd310
amitganvir <agavir@gmail.com>
2019-09-14 11:23:04 -0500

    Start Commit

---
+++ src/main/java/com/www/campsitereservations/CampsitereservationsApplication.java
@@ -0,0 +1,13 @@
+package com.www.campsitereservations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampsitereservationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampsitereservationsApplication.class, args);
+    }
+
+}
